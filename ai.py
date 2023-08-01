a62c03317c9d543d5cb22168370617c6495016e2
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-01 12:33:55 +1200

    version 6 ai.py
    the rgb checking is now done using c code and it now uses 40 of each category

--- ai.py
+++ ai.py
@@ -1,46 +1,72 @@
-def rgb_checker(pixel, testpixel, score):
+from ctypes import *
+import ctypes
+c_file = "./my_functions.so"
+rgb = CDLL(c_file)
+
+def rgb_checker(pixel, testpixel):
+    array = (ctypes.c_int * 3)(*pixel)
+    testarray = (ctypes.c_int * 3)(*testpixel)
+    score_add = rgb.rgb_checker(array, testarray)
+    round(score_add)
+    return score_add
+
+
+def pixel_checker(row, testrow):
     x = 0
     score_add = 0
-    while x < 3:
-        if round(pixel[x]/3) == round(testpixel[x]/3):
-            score_add += 1
-        x += 1
-    return score + score_add
-
-def pixel_checker(row, testrow, score):
-    x = 0
     while x < 99:
-        score += rgb_checker(row[x], testrow[x], score)
+        score_add += rgb_checker(row[x], testrow[x])
         x += 1
-    return score
+    return score_add
 
-def row_checker(image, testimage, score):
+def row_checker(image, testimage):
     x = 0
+    score_add = 0
     while x < 99:
-        score += pixel_checker(image[x], testimage[x], score)
+        score_add += pixel_checker(image[x], testimage[x])
         x += 1
-    return score
+    return score_add
 
-import numpy as np
+# Importing libraries
 import cv2
-import glob
+
+import matplotlib.pyplot as plt
+import numpy as np
 import os
-import sys
+import PIL
 import time
+
 x = 1
 cat = []
 dog = []
-while x < 11:
+while x < 41:
     cat += [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x) + ".png"))]
     dog += [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x) + ".png"))]
     x += 1
+
 test = cv2.imread(cv2.samples.findFile("test_set/cats/test.png"))
 catidea = 0
+catidea += row_checker(cat[0], test)
+catidea += row_checker(cat[1], test)
+catidea += row_checker(cat[2], test)
+catidea += row_checker(cat[3], test)
+catidea += row_checker(cat[4], test)
+catidea += row_checker(cat[5], test)
+catidea += row_checker(cat[6], test)
+catidea += row_checker(cat[7], test)
+catidea += row_checker(cat[8], test)
+catidea += row_checker(cat[9], test)
 dogidea = 0
-x = 0
-while x < len(cat):
-    catidea = row_checker(cat[x], test, catidea)
-    dogidea = row_checker(dog[x], test, dogidea)
+dogidea += row_checker(dog[0], test)
+dogidea += row_checker(dog[1], test)
+dogidea += row_checker(dog[2], test)
+dogidea += row_checker(dog[3], test)
+dogidea += row_checker(dog[4], test)
+dogidea += row_checker(dog[5], test)
+dogidea += row_checker(dog[6], test)
+dogidea += row_checker(dog[7], test)
+dogidea += row_checker(dog[8], test)
+dogidea += row_checker(dog[9], test)
 if catidea >= dogidea:
     time.sleep(1)
     print('cat')
@@ -49,8 +75,8 @@ if dogidea >= catidea:
     print('dog')
 if catidea == dogidea:
     time.sleep(1)
-    print('ÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂsÄÂĂÂÄÂĂÂĂÂĂÂÄÂĂÂÄÂĂÂ~ÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂ>ÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂÄÂĂÂĂÂĂÂÄÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂÄÂĂÂĂÂĂÂÄÂĂÂdÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂ{ÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂÄÂĂÂĂÂĂÂĂĹĄĂËÄÂĂÂÄÂĂÂĂÂĂÂÄÂÄšĹžÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂ ÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂ1ÄÂĂÂÄÂĂÂÄÂÄšÄÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂÄÂĂÂĂĹĄÄšÄ˝ĂÂĂÂĂĹĄÄšĹĂÂĂÂÄÂĂÂ')
+    print("error")
 print("")
 print(catidea)
 print('\n')
-print(dogidea))
+print(dogidea)
