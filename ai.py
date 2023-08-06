8d97b92ad6f9a116b96965d7249583143fe4cd7b
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-07 08:51:35 +1200

    version 7 ai.py
    fixed it loading 82 training images but still using 40
    increased training data amount to 400
    merged c caller into pixel checker

--- ai.py
+++ ai.py
@@ -3,19 +3,11 @@ import ctypes
 c_file = "./my_functions.so"
 rgb = CDLL(c_file)
 
-def rgb_checker(pixel, testpixel):
-    array = (ctypes.c_int * 3)(*pixel)
-    testarray = (ctypes.c_int * 3)(*testpixel)
-    score_add = rgb.rgb_checker(array, testarray)
-    round(score_add)
-    return score_add
-
-
 def pixel_checker(row, testrow):
     x = 0
     score_add = 0
     while x < 99:
-        score_add += rgb_checker(row[x], testrow[x])
+        score_add += rgb.rgb_checker((ctypes.c_int * 3)(*row[x]), (ctypes.c_int * 3)(*testrow[x]))
         x += 1
     return score_add
 
@@ -39,42 +31,24 @@ import time
 x = 1
 cat = []
 dog = []
-while x < 41:
+while x < 201:
     cat += [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x) + ".png"))]
     dog += [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x) + ".png"))]
     x += 1
 
-test = cv2.imread(cv2.samples.findFile("test_set/cats/test.png"))
+test = cv2.imread(cv2.samples.findFile(input("\nwhere is test image: ")))
 catidea = 0
-catidea += row_checker(cat[0], test)
-catidea += row_checker(cat[1], test)
-catidea += row_checker(cat[2], test)
-catidea += row_checker(cat[3], test)
-catidea += row_checker(cat[4], test)
-catidea += row_checker(cat[5], test)
-catidea += row_checker(cat[6], test)
-catidea += row_checker(cat[7], test)
-catidea += row_checker(cat[8], test)
-catidea += row_checker(cat[9], test)
 dogidea = 0
-dogidea += row_checker(dog[0], test)
-dogidea += row_checker(dog[1], test)
-dogidea += row_checker(dog[2], test)
-dogidea += row_checker(dog[3], test)
-dogidea += row_checker(dog[4], test)
-dogidea += row_checker(dog[5], test)
-dogidea += row_checker(dog[6], test)
-dogidea += row_checker(dog[7], test)
-dogidea += row_checker(dog[8], test)
-dogidea += row_checker(dog[9], test)
+x = 0
+while x < 200:
+    catidea += row_checker(cat[x], test)
+    dogidea += row_checker(dog[x], test)
+    x += 1
 if catidea >= dogidea:
-    time.sleep(1)
     print('cat')
 if dogidea >= catidea:
-    time.sleep(1)
     print('dog')
 if catidea == dogidea:
-    time.sleep(1)
     print("error")
 print("")
 print(catidea)
