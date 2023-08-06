494fa73b174051bdc43d03b69d5f06adf1a4c239
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-07 09:11:39 +1200

    Update my_functions.c
    switched from float 1/3 to int 1
    unrolled loop
    made branchless

--- my_functions.c
+++ my_functions.c
@@ -1,19 +1,13 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 // Function to check the similarity of RGB values
 float rgb_checker(unsigned char *pixel, unsigned char *testpixel) {
     int x;
-    float score_add = 0;
-    for (x = 0; x < 3; x++) {
-        if (roundf(pixel[x]/5) == roundf(testpixel[x]/5)) {
-            score_add += 1.0 / 3.0;
-        }
-    }
-    return roundf(score_add);
+    int score_add = 0;
+    score_add += (roundf(pixel[0]/5) == roundf(testpixel[0]/5));
+    score_add += (roundf(pixel[1]/5) == roundf(testpixel[1]/5));
+    score_add += (roundf(pixel[2]/5) == roundf(testpixel[2]/5));
+    return score_add;
 }
