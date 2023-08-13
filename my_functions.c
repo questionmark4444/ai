c3a3800cd2dab11bad017ef79c063518977cf790
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-14 09:00:30 +1200

    Update my_functions.c
    now it processes the rows that are now easier to process

--- my_functions.c
+++ my_functions.c
@@ -4,10 +4,9 @@
 
 // Function to check the similarity of RGB values
 float rgb_checker(unsigned char *pixel, unsigned char *testpixel) {
-    int x;
-    int score_add = 0;
-    score_add += (roundf(pixel[0]/5) == roundf(testpixel[0]/5));
-    score_add += (roundf(pixel[1]/5) == roundf(testpixel[1]/5));
-    score_add += (roundf(pixel[2]/5) == roundf(testpixel[2]/5));
-    return score_add;
+    int add_score = 0;
+    for (int x = 0; x < 101; x++) {
+        add_score += ((int)(roundf((pixel[x])*0.2) == roundf((testpixel[x])*0.2)));
+    }
+    return add_score;
 }
