2136981193c17148d6bdcb807e811d494c583dee
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-14 08:59:23 +1200

    version 8 ai.py
    added more convolutional processing for rgb values
    fixed it only taking 99 out of 100 rows of an image
    fixed it only taking 99 out of 100 pixels of a row

--- ai.py
+++ ai.py
@@ -3,19 +3,42 @@ import ctypes
 c_file = "./my_functions.so"
 rgb = CDLL(c_file)
 
+def rgb_processor(row):
+    x = 0
+    newvalue = 0
+    newvalue1 = 0
+    newvalue2 = 0
+    newvalue3 = 0
+    newrow = [0] * 100
+    while x < 100:
+        newvalue1 = row[x][0]/3
+        newvalue2 = row[x][1]/3
+        newvalue3 = row[x][2]/3
+        newvalue = round(newvalue1+newvalue2+newvalue3)
+        newrow[x] = newvalue
+        x += 1
+    return newrow
+
 def pixel_checker(row, testrow):
     x = 0
     score_add = 0
-    while x < 99:
-        score_add += rgb.rgb_checker((ctypes.c_int * 3)(*row[x]), (ctypes.c_int * 3)(*testrow[x]))
+    while x < 100:
+        score_add += rgb.rgb_checker((ctypes.c_int * 100)(*row), (ctypes.c_int * 100)(*testrow))
         x += 1
-    return score_add
+    return score_add*0.2
 
 def row_checker(image, testimage):
     x = 0
     score_add = 0
-    while x < 99:
-        score_add += pixel_checker(image[x], testimage[x])
+    newimage = [0] * 100
+    newtestimage = [0] * 100
+    while x < 100:
+        newimage[x] = rgb_processor(image[x])
+        newtestimage[x] = rgb_processor(testimage[x])
+        x += 1
+    x = 0
+    while x < 100:
+        score_add += pixel_checker(newimage[x], newtestimage[x])
         x += 1
     return score_add
 
@@ -29,9 +52,9 @@ import PIL
 import time
 
 x = 1
-cat = []
-dog = []
-while x < 201:
+cat = [[[[0, 0, 0]] * 100] * 100] * 600
+dog = [[[[0, 0, 0]] * 100] * 100] * 600
+while x < 801:
     cat += [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x) + ".png"))]
     dog += [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x) + ".png"))]
     x += 1
@@ -40,7 +63,7 @@ test = cv2.imread(cv2.samples.findFile(input("\nwhere is test image: ")))
 catidea = 0
 dogidea = 0
 x = 0
-while x < 200:
+while x < 800:
     catidea += row_checker(cat[x], test)
     dogidea += row_checker(dog[x], test)
     x += 1
