ad6c4e60b9ca47f393785754f50aa45e6ee135dd
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-29 10:46:39 +1200

    version 9 ai.py
    can used stored preprocessed data, and can handle all the images at once in a few minutes

--- ai.py
+++ ai.py
@@ -1,79 +1,69 @@
 from ctypes import *
 import ctypes
+import pandas as pd
+import csv
+
+#c functions
 c_file = "./my_functions.so"
 rgb = CDLL(c_file)
 
-def rgb_processor(row):
-    x = 0
-    newvalue = 0
-    newvalue1 = 0
-    newvalue2 = 0
-    newvalue3 = 0
-    newrow = [0] * 100
-    while x < 100:
-        newvalue1 = row[x][0]/3
-        newvalue2 = row[x][1]/3
-        newvalue3 = row[x][2]/3
-        newvalue = round(newvalue1+newvalue2+newvalue3)
-        newrow[x] = newvalue
-        x += 1
-    return newrow
-
-def pixel_checker(row, testrow):
-    x = 0
-    score_add = 0
-    while x < 100:
-        score_add += rgb.rgb_checker((ctypes.c_int * 100)(*row), (ctypes.c_int * 100)(*testrow))
-        x += 1
-    return score_add*0.2
-
+#compare test image with data
 def row_checker(image, testimage):
-    x = 0
     score_add = 0
-    newimage = [0] * 100
-    newtestimage = [0] * 100
-    while x < 100:
-        newimage[x] = rgb_processor(image[x])
-        newtestimage[x] = rgb_processor(testimage[x])
-        x += 1
-    x = 0
-    while x < 100:
-        score_add += pixel_checker(newimage[x], newtestimage[x])
-        x += 1
+    notsaved = bool(0)
+    if notsaved:
+        #compare images
+        newimage = np.array([[rgb.rgb_processor((ctypes.c_int * 3)(*image[x][i])) for i in range(100)] for x in range(100)])
+        newtestimage = np.array([[rgb.rgb_processor((ctypes.c_int * 3)(*testimage[x][i])) for i in range(100)] for x in range(100)])
+        for x in range(100):
+            score_add += rgb.rgb_checker((ctypes.c_int * 100)(*newimage[x]), (ctypes.c_int * 100)(*newtestimage[x]))
+    else:
+        #compare excel files
+        image_array = np.array(image)  # Convert the list to a NumPy array
+        image_uint8 = image_array.astype(np.uint8)
+        score_add = np.sum(np.equal(image_uint8, testimage[:, :, 0]))
     return score_add
 
 # Importing libraries
 import cv2
-
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
 import time
 
-x = 1
-cat = [[[[0, 0, 0]] * 100] * 100] * 600
-dog = [[[[0, 0, 0]] * 100] * 100] * 600
-while x < 801:
-    cat += [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x) + ".png"))]
-    dog += [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x) + ".png"))]
-    x += 1
-
+#if values have been preprocessed and saved in excel files
+saved = bool(1)
+image_number = 5000
+cat = []
+dog = []
+if saved:
+    #open excel files
+    for x in range(image_number):
+        with open('temp/cat' + str(x+1) + '.csv', 'r') as f:
+            cat += [list(csv.reader(f))]
+        with open('temp/dog' + str(x+1) + '.csv', 'r') as f:
+            dog += [list(csv.reader(f))]
+else:
+    #open images
+    cat = [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x+1) + ".png")) for x in range(image_number)]
+    dog = [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x+1) + ".png")) for x in range(image_number)]
 test = cv2.imread(cv2.samples.findFile(input("\nwhere is test image: ")))
+
+x = 0
 catidea = 0
 dogidea = 0
-x = 0
-while x < 800:
+#compare test image with data
+for x in range(image_number):
     catidea += row_checker(cat[x], test)
     dogidea += row_checker(dog[x], test)
-    x += 1
 if catidea >= dogidea:
     print('cat')
 if dogidea >= catidea:
     print('dog')
 if catidea == dogidea:
     print("error")
-print("")
+print('\n')
 print(catidea)
 print('\n')
 print(dogidea)
