abaf8757e0b96aaf96e6841227109bca51e63dcf
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-29 10:47:31 +1200

    new c function
    processes rgb values

--- my_functions.c
+++ my_functions.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Function to merge RGB values
+float rgb_processor(unsigned char *row) {
+    return (row[0]+row[1]+row[2])*0.33333333333;
+}
+
 // Function to check the similarity of RGB values
 float rgb_checker(unsigned char *pixel, unsigned char *testpixel) {
     int add_score = 0;
-    for (int x = 0; x < 101; x++) {
-        add_score += ((int)(roundf((pixel[x])*0.2) == roundf((testpixel[x])*0.2)));
+    for (int x = 0; x < 101; ++x) {
+        add_score += ((int)(roundf(pixel[x]*0.0196078431) == roundf(testpixel[x]*0.0196078431)));
     }
     return add_score;
 }
