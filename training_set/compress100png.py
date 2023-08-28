30c718b14cf6016e0a1f11c793a6a96877703c71
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-08-29 10:51:13 +1200

    Update compress100png.py

--- training_set/compress100png.py
+++ training_set/compress100png.py
@@ -4,13 +4,16 @@ from PIL import Image
 import time
 
 x = 1
-while (x < 501) :
-        #im1 = Image.open(f'dog.{x}.jpg')
-        im1 = Image.open(f'dogs/{x}.png')
-        im2 = im1.resize((100, 100))
+while (x < 5000) :
+        im1 = Image.open(f'cats/{x}.png')
+        im2 = Image.open(f'dogs/{x}.png')
+        im3 = im1.resize((100, 100))
+        im4 = im2.resize((100, 100))
         try:
-                im2.save(f'dogs/{x}.png', optimize = True, quality=99)
+                im3.save(f'cats/{x}.png', format="PNG", optimize = True, quality=99)
+                im4.save(f'dogs/{x}.png', format="PNG", optimize = True, quality=99)
         except:
+                im1.save(f'cats/{x}.png')
                 im2.save(f'dogs/{x}.png')
                 print(x)
         time.sleep(0.1)
