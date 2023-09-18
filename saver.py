ba62ad17409f4a631240d8b73f06218036cc09b7
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-09-18 18:15:05 +1200

    Update saver.py
    better error handling and more input

--- saver.py
+++ saver.py
@@ -1,7 +1,7 @@
+#libraries
 from ctypes import *
 import ctypes
 import csv
-
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,23 +9,60 @@ import os
 import PIL
 import time
 
+
+#variable to record how long this will take to load c functions
+RecordStart1 = time.time()
+
+
 #import c functions
 c_file = "./my_functions.so"
 rgb = CDLL(c_file)
 
-number_images = 5000
 
-#import images
-cat = [cv2.imread(cv2.samples.findFile("training_set/cats/" + str(x+1) + ".png")) for x in range(number_images)]
-dog = [cv2.imread(cv2.samples.findFile("training_set/dogs/" + str(x+1) + ".png")) for x in range(number_images)]
+#other variable to record how long this will take to load c functions
+Recordend1 = time.time() - RecordStart1
 
-#process data
-newcat = [[[rgb.rgb_processor((ctypes.c_int * 3)(*cat[y][x][i])) for i in range(100)] for x in range(100)] for y in range(number_images)]
-newdog = [[[rgb.rgb_processor((ctypes.c_int * 3)(*dog[y][x][i])) for i in range(100)] for x in range(100)] for y in range(number_images)]
 
-#save data in excel files
-for x in range(number_images):
-    with open(('temp/cat' + str(x+1) + '.csv'), 'w') as f:
-        csv.writer(f).writerows(newcat[x])
-    with open(('temp/dog' + str(x+1) + '.csv'), 'w') as f:
-        csv.writer(f).writerows(newdog[x])
+#get number of images from user
+loop = true
+while loop:
+    try: # in case the image could not be found
+        number_images = int(input("number of images of each class: "))
+        print("") #new line
+        loop = false
+    except:
+        print("sorry something went wrong please try again")
+
+
+#input classes
+try: #in case the images could not be found
+    print("what is the first class of images")
+    print("(this will look for folder with the name of that class and png images with ascending number names)")
+    class1 = input(" : ")
+    class2 = input("what is the second class of images: ")
+    print("") #new line
+
+    #variable to record how long this will take to load and process images
+    RecordStart2 = time.time()
+
+    #import images
+    class1_data = [cv2.imread(cv2.samples.findFile(f"training_set/{class1}/{x+1}.png")) for x in range(number_images)]
+    class2_data = [cv2.imread(cv2.samples.findFile(f"training_set/{class2}/{x+1}.png")) for x in range(number_images)]
+
+    #process data
+    newclass1_data = [[[rgb.rgb_processor((ctypes.c_int * 3)(*class1_data[y][x][i])) for i in range(100)] for x in range(100)] for y in range(number_images)]
+    newclass2_data = [[[rgb.rgb_processor((ctypes.c_int * 3)(*class2_data[y][x][i])) for i in range(100)] for x in range(100)] for y in range(number_images)]
+
+    #save data in excel files
+    for x in range(number_images):
+        with open((f'temp/{class1}{x}.csv'), 'w') as f:
+            csv.writer(f).writerows(newclass1_data[x])
+        with open((f'temp/{class2}{x}.csv'), 'w') as f:
+            csv.writer(f).writerows(newclass2_data[x])
+
+    #other variable to record how long this will take to load and process images
+    Recordend2 = time.time() - RecordStart2
+    print(int((Recordend1 + Recordend2) / 60 * 100) / 100)  #print amount of minutes it took with 2 decimal places
+except: 
+    print("sorry could not find images of that class")
+    print("(please write the name of the folder correctly and name the images correctly)")
