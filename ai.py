001f7ea0707e30e6f5a999c846b403e7f9894387
questionmark4444 <139928629+questionmark4444@users.noreply.github.com>
2023-10-11 10:36:34 +1300

    Update ai.py
    fixed improper capitalisation in true and false
    it not using number_images
    comment using " " " caused error so replaced with #

--- ai.py
+++ ai.py
@@ -10,33 +10,37 @@ import ctypes
 tf.config.set_visible_devices([], 'GPU')
 
 
-#input classes
-loop = true
-while loop:  #in case couldnt find excel data
-    class1 = input('what is the first class of image you want to classify')
-    class2 = input('what is the second class of image you want to classify')
-
-    # Load Excel data for training
-    try:
-        class1_data = [np.loadtxt(f'temp/{class1}{x+1}.csv', delimiter=',') for x in range(5000)]
-        class2_data = [np.loadtxt(f'temp/{class2}{x+1}.csv', delimiter=',') for x in range(5000)]
-        loop = false
-    except:
-        print("sorry, couldn't find the saved files")
-
-
 # Import c functions
 c_file = "./my_functions.so"
 rgb = ctypes.CDLL(c_file)
+
+
+#input classes
+loop = True
+while loop:    #in case couldnt find excel data
+    try:    
+        print("")  #new line
+        number_of_images = int(input('what number of images of each class do you want to use: '))
+        print("")
+        print("(this will look for a folder with the name of that class and png images with ascending number names)")
+        class1 = input('what is the first class of image you want to classify: ')
+        class2 = input('what is the second class of image you want to classify: ')
+
+        # Load Excel data for training
+        class1_data = [np.loadtxt(f'temp/{class1}{x+1}.csv', delimiter=',') for x in range(number_of_images)]
+        class2_data = [np.loadtxt(f'temp/{class2}{x+1}.csv', delimiter=',') for x in range(number_of_images)]
+        loop = False
+    except:
+        print("sorry, couldn't find the saved files, please try again")
 
 
 # Function to load and preprocess image from file using C library
 def load_and_preprocess_image(file_path):
     image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)  # Load as grayscale (black and white)
 
-    image = cv2.resize(image, (100, 100))                """each pixel in the loaded image is a list of 3 values, 
-                                                            in grayscale the are made the same, 
-                                                            this will resize and replace the lists with the single values"""
+    image = cv2.resize(image, (100, 100))                #each pixel in the loaded image is a list of 3 values, 
+                                                         #in grayscale the are made the same, 
+                                                         #this will resize and replace the lists with the single values"""
 
     image = image / 255.0                                # make each value a decimal between 0 and 1 to make comparing easier
     image = np.expand_dims(image, axis=-1)               # Add a channel dimension
@@ -45,12 +49,12 @@ def load_and_preprocess_image(file_path):
 
 
 # Load and preprocess the test image using C library
-loop = true
+loop = True
 while loop:  #in case couldnt find image
     test_image_path = input("\nWhere is the test image: ")
     try:
         test_image = load_and_preprocess_image(test_image_path)
-        loop = false
+        loop = False
     except:
         print("sorry, couldn't find the image")
 
@@ -74,13 +78,13 @@ model.add(layers.Flatten())
 
 # Dense layers
 model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dense(2, activation='softmax'))  # Two output classes: cat and dog
+model.add(layers.Dense(2, activation='softmax'))  # Two output classes: cat and dog as example
 
 # Compile the model
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Convert labels to NumPy array
-train_labels = np.array([0] * len(class1_data) + [1] * len(class2_data))  # 0 for cat, 1 for dog
+train_labels = np.array([0] * len(class1_data) + [1] * len(class2_data))  # 0 for cat, 1 for dog as example
 
 # Train the model with your cat and dog data
 train_images = np.concatenate([class1_data, class2_data], axis=0)
